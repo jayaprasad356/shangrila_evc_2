1a0d6f33fff0692c1dd11a96970f30651aaa1be4
divaharsha <divakarvan03@gmail.com>
2023-01-03 11:10:46 +0530

    initial

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\CentralLogics\Helpers;
+use App\Http\Controllers\Controller;
+use App\Model\Admin;
+use App\Model\Branch;
+use App\Model\Income;
+use App\Model\Category;
+use App\Model\Order;
+use App\Model\OrderDetail;
+use App\Model\Product;
+use App\Model\Review;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    public function fcm($id)
+    {
+        $fcm_token = Admin::find(auth('admin')->id())->fcm_token;
+        $data = [
+            'title' => 'New auto generate message arrived from admin dashboard',
+            'description' => $id,
+            'order_id' => '',
+            'image' => '',
+            'type'=>'order_status',
+        ];
+        Helpers::send_push_notif_to_device($fcm_token, $data);
+
+        return "Notification sent to admin";
+    }
+
+    public function dashboard()
+    {
+       
+
+        $data = self::order_stats_data();
+
+        $data['customer'] = User::count();
+
+        return view('admin-views.dashboard', compact('data'));
+    }
+
+    public function order_stats(Request $request)
+    {
+        session()->put('statistics_type', $request['statistics_type']);
+        $data = self::order_stats_data();
+
+        return response()->json([
+            'view' => view('admin-views.partials._dashboard-order-stats', compact('data'))->render()
+        ], 200);
+    }
+
+    public function order_stats_data() {
+        $today = session()->has('statistics_type') && session('statistics_type') == 'today' ? 1 : 0;
+        $this_month = session()->has('statistics_type') && session('statistics_type') == 'this_month' ? 1 : 0;
+
+    }
+
+
+}
---
+++ resources/views/admin-views/partials/_dashboard-order-stats.blade.php
@@ -0,0 +1,25 @@
+
+<div class="col-sm-6 col-lg-4 mb-3 mb-lg-5">
+    <!-- Card -->
+    <a class="card card-hover-shadow h-100" href="" style="background: #3E215D">
+    <!-- <a class="card card-hover-shadow h-100" href="{{'admin/client/add'}}" style="background: #3E215D"> -->
+        <div class="card-body">
+            <h6 class="card-subtitle"
+                style="color: white!important;">{{translate('clients')}}</h6>
+            <div class="row align-items-center gx-2 mb-1">
+                <div class="col-6">
+                                    <span class="card-title h2" style="color: white!important;">
+                                    2000
+                                    </span>
+                </div>
+                <div class="col-6 mt-2">
+                    <i class="tio-user ml-6" style="font-size: 30px;color: white"></i>
+                </div>
+            </div>
+            <!-- End Row -->
+        </div>
+    </a>
+    <!-- End Card -->
+</div>
+
+
